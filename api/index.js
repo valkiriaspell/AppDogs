583b92a4833a8b9a9647936a1eeb61e434a3ad06
Valkiria Salerno <81522778+valkiriaspell@users.noreply.github.com>
2022-03-18 16:27:15 -0300

    deploy
    fixing

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ const {saveTemps} = require ('./src/api-calls/temps.js')
 // });
 
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT, async () => {
     const preload = await saveTemps()
     console.log('El servidor esta escuchando en el puerto 3001'); // eslint-disable-line no-console
   });
